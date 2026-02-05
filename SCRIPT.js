dc0a8787c5d4df0e94acd67e7d986af47c3d7794
christianjaycruiz-netizen <christianjayc.ruiz@gmail.com>
2026-02-05 22:26:02 +0800

    Add files via upload

---
+++ SCRIPT.js
@@ -0,0 +1,33 @@
+const container = document.getElementById('particle-container');
+const emojis = ['💖','💘','❤️','💕','🌹','✨'];
+
+function createParticle() {
+    const p = document.createElement('div');
+    p.className = 'particle';
+    p.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+    p.style.left = Math.random() * 100 + 'vw';
+    p.style.fontSize = Math.random() * 30 + 20 + 'px';
+
+    const duration = Math.random() * 3 + 3;
+    p.style.setProperty('--duration', duration + 's');
+
+    container.appendChild(p);
+    setTimeout(() => p.remove(), duration * 1000);
+}
+
+setInterval(createParticle, 200);
+
+/* BUTTON LOGIC */
+const noBtn = document.getElementById('noBtn');
+const yesBtn = document.getElementById('yesBtn');
+let scale = 1;
+
+noBtn.addEventListener('click', () => {
+    scale -= 0.15;
+    if (scale < 0.2) scale = 0.2;
+    noBtn.style.transform = `scale(${scale})`;
+});
+
+yesBtn.addEventListener('click', () => {
+    window.location.href = "yes.html";
+});
